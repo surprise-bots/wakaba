d7ace91386c37778ed16c04395f4348dcf442a60
surprise associate <surpriseassociate@gmail.com>
2021-10-29 09:35:24 +1000

    Intents misspell

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ const rest = new REST({ version: '9' }).setToken('token');
 )();
 
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUIILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on('ready', () =>{
     console.log('Logged in as ${client.user.tag}');
