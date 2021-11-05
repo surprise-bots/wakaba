fe0901fb9ba583772e2503615f5c9f8159d8337c
Surprise Associate <surprise.associate@gmail.com>
2021-11-05 20:15:38 +1100

    Making debugging notice more obvious in terminal

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 # I am not fucking bothered to fuck with ENTRYPOINTS and shit so here's a script. Enjoy.
 
-echo $BOT_TOKEN
+echo "BOT TOKEN: " $BOT_TOKEN
 /usr/bin/git pull
 /usr/local/bin/node bot.js
