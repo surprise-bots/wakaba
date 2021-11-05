c5bd4b9e877470068748a2500cc07c2a537695c7
Surprise Associate <surprise.associate@gmail.com>
2021-11-05 21:50:24 +1100

    Changing structure of return

--- commands/excuse.js
+++ commands/excuse.js
@@ -6,7 +6,9 @@ module.exports = {
         .setDescription('Provides a totally real excuse for IT problems.'),
     async execute(interaction) {
         con.query("SELECT saying FROM bofh_sayings ORDER BY RAND() LIMIT 1", (err, row) => {
-            return interaction.reply(row);
+            retres = row;
         });
+
+        return interaction.reply(retres);
     },
 };
