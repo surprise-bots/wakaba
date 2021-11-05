758f8a8a1d580894357bb2e12d6da5ac0a1223ab
Surprise Associate <surprise.associate@gmail.com>
2021-11-05 20:37:31 +1100

    reverting back to a semi cogged version

--- bot.js
+++ bot.js
@@ -1,14 +1,38 @@
-// Require the neccersary discord.js classes
-const { Client, Intents } = require('discord.js');
-const { token } = process.env.BOT_TOKEN;
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { REST }   = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const token      = process.env.BOT_TOKEN;
+const clientId   = process.env.CLIENT_ID;
+const guildId    = process.env.GUILD_ID;
 
-// Create instance of the client
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When client is ready and online, run some code...
-client.once('ready', ()=> {
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
     console.log('Ready!');
 });
 
-// Login to Discord using the bots token
-client.login(token)
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
+
+client.login(token);
