9fcfdcc697473f62e1403b671ca1f28a6fbf4c4e
Surprise Associate <surprise.associate@gmail.com>
2021-11-07 11:41:07 +1100

    Updating deploy command script because i think its needed

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
-const { REST } = require('@discordjs/rest');
+const fs         = require('fs');
+const { REST }   = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { token } = process.env.BOT_TOKEN;
-const { clientId } = process.env.CLIENT_ID;
-const { guildId } = process.env.GUILD_ID;
+const token      = process.env.BOT_TOKEN;
+const clientId   = process.env.CLIENT_ID;
+const guildId    = process.env.GUILD_ID;
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
