592d4cb11b8f971e437b5ea268a6c165d1e065de
Surprise Associate <surprise.associate@gmail.com>
2021-11-07 11:54:52 +1100

    Move test script into deploy-commands as its confirmed working

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,20 +1,39 @@
-const fs         = require('fs');
-const { REST }   = require('@discordjs/rest');
+const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const fs = require('fs');
+
+// Required Variables
 const token      = process.env.BOT_TOKEN;
 const clientId   = process.env.CLIENT_ID;
 const guildId    = process.env.GUILD_ID;
+const mycnf = {
+    "host" : process.env.MYSQL_HOST,
+    "user" : process.env.MYSQL_USER,
+    "password" : process.env.MYSQL_PASS,
+    "database" : process.env.MYSQL_DB
+};
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-    .then(() => console.log('Successfully registered application commands.'))
-    .catch(console.error);
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
