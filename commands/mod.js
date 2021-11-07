09cc3c121c247a9eeb1345468a4071c5006bd175
Surprise Associate <surprise.associate@gmail.com>
2021-11-07 17:33:38 +1100

    trying shit

--- commands/mod.js
+++ commands/mod.js
@@ -8,17 +8,13 @@ module.exports = {
             subcommand
                 .setName('kick')
                 .setDescription('Pretend to kick a user')
-                .addUserOption(option => option.setName('target').setDescription('Mention the user'), async execute(interaction){
-                    return interaction.reply('Test')
-                })
+                .addUserOption(option => option.setName('target').setDescription('Mention the user'))
         )
         .addSubcommand(subcommand => 
             subcommand
                 .setName('anothersubcommand')
                 .setDescription('Just something extra')
-                .addUserOption(option => option.setName('target').setDescription('Mention the user'), async execute(interaction){
-                    return interaction.reply('Test')
-                })
+                .addUserOption(option => option.setName('target').setDescription('Mention the user'))
         ),
 	async execute(interaction) {
 		return interaction.reply({ content: `A set of moderations tools. Does nothing on it's own.` });
