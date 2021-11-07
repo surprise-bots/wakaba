b83ef76874289562614639a2d45bd452f1af2d44
Surprise Associate <surprise.associate@gmail.com>
2021-11-07 13:11:41 +1100

    asd

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
+    client.commands.push(command.data.toJSON());
     client.commands.set(command.data.name, command);
 }
 
