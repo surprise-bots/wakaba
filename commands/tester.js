c45834d563858fc93062308391c9bb6bcebec812
surprise associate <surpriseassociate@gmail.com>
2021-11-12 08:40:55 +1000

    Adding tester command

---
+++ commands/tester.js
@@ -0,0 +1,9 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	name: 'ping',
+	description: 'Ping!',
+	execute(message, args) {
+		message.channel.send('Pong.');
+	},
+};
